77be1ba009105b7267c01e361d326c64532eee44
Tim Filmer <tim.filmer@gmail.com>
2017-04-30 11:23:48 +1000

    macarthur working

--- prediction.js
+++ prediction.js
@@ -16,22 +16,22 @@ function calcDistance(lat1, lat2, lon1, lon2) {
   var dLat = (lat2 - lat1) * Math.PI/180;
   var dLon = (lon2 - lon1) * Math.PI/180;
   var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-          Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+          Math.cos(lat1 * Math.PI/180) * Math.cos(lat2 * Math.PI/180) *
           Math.sin(dLon / 2) * Math.sin(dLon / 2);
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   var d = R * c;
   return d;
 }
 
-
-//φ is latitude, λ is longitude, θ is the bearing (clockwise from north), δ is the angular distance d/R; d being the distance travelled, R the earth’s radius
-
 function calcNewPosition(lat1, lon1, bearing, distance) {
   var R = 6371000; // m
-  var lat2 = Math.asin( Math.sin(lat1)*Math.cos(distance/R) + Math.cos(lat1)*Math.sin(distance/R)*Math.cos(bearing) );
-  var lon2 = lon1 + Math.atan2(Math.sin(bearing)*Math.sin(distance/R)*Math.cos(lat1), Math.cos(distance/R)-Math.sin(lat1)*Math.sin(lat2));
-  return {lat2, lon2};
-
+  var lat2 = Math.asin( Math.sin(lat1 * Math.PI/180)*Math.cos(distance/R) +
+            Math.cos(lat1 * Math.PI/180)*Math.sin(distance/R)*
+            Math.cos(bearing * Math.PI/180) );
+  var lon2 = lon1 + Math.atan2(Math.sin(bearing * Math.PI/180)*Math.sin(distance/R)*
+            Math.cos(lat1 * Math.PI/180), Math.cos(distance/R)-
+            Math.sin(lat1* Math.PI/180)*Math.sin(lat2 * Math.PI/180));
+  return [lat2, lon2];
 }
 
 function readCSV(path) {
@@ -45,8 +45,16 @@ function getValue(lat, lon, array) {
   return array[Math.floor(lat)][Math.floor(lon)];
 }
 
-function rate(slope, temp, humidity, windSpeed) {
-  return 0;
+function getRate(slopeFactor, temp, humidity, windSpeed) {
+  var h = 5; // drought factor
+  var c = humidity;
+  var d = windSpeed * 3.6; // km/h
+  var b = temp - 273; // degrees c
+  var fuelLoad = 12.5; // estimated
+
+  var k=2*(Math.exp((0.987*Math.log(h+0.001))-0.45-(0.0345*c)+(0.0338*b)+(0.0234*d)));
+
+  return rate = 0.0012 * k * slopeFactor * fuelLoad;
 }
 
 function getElevation() {
@@ -59,11 +67,18 @@ function drawShape() {
 
 }
 
-function getSlope(lat1, lat2, lon1, lon2) {
+function getSlope(lat1, lon1) {
   var slope;
 
-
   return slope;
+}
+
+function getSlopeFactor(angle) {
+  if(angle >= 0) {
+    return Math.exp(0.069 * angle)
+  } else {
+    return Math.exp(-0.069 * angle) / (2 * Math.exp(-0.069 * angle) - 1)
+  }
 }
 
 function predict(lat, lon, timeDifference) {
@@ -73,10 +88,21 @@ function predict(lat, lon, timeDifference) {
   temp = getValue(lat, lon, tempArray);
   windU = getValue(lat, lon, windUArray);
   windV = getValue(lat, long, windVArray);
-
+  var angle = 0;
   // get slopes
 
-  // get rates
+  var slopes = [];
 
-  // draw shape
+  for(var i = 0; i < 8; ++i) {
+    // get slopes
+
+    // get rates
+
+    // get point as lat, lon
+  }
 }
+exports.test = function () {
+  var sf = getSlopeFactor(5);
+  var rate = getRate(sf, 303, 50, 10);
+  console.log("sf: " + sf + ", rate: " + rate);
+};
